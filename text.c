a7655350a54c540c0bde29eb9b2dc8a6cd5abdcb
Maneesh7781 <maneesh.suggala@s.amity.edu>
2025-02-12 14:24:49 +0530

    i added mul sub and sub

--- text.c
+++ text.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 int main(){
-	float a=10,b=20,sum;
+	float a=10,b=20,sum,sub,mul,divi;
 	sum=a+b;
+	sub=a-b;
+	divi=a/b;
+	mul=a*b;
 	printf("%f",sum);
+	printf("%f",sub);
+	printf("%f",mul);
+	printf("%f",divi);
 	return 0;
 }
